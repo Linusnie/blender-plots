39d155d3828fbfc95db78d5a3cea80cca56557f0
Linus Härenstam-Nielsen <linusnie@kth.se>
2023-04-22 11:09:59 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='blender-plots',
+    version='1.0',
+    author='Linus Härenstam-Nielsen',
+    description='Plotting API for Blender',
+    url='https://github.com/Linusnie/blender-plots',
+    python_requires='>=3.10',
+    packages=['blender_plots'],
+)
